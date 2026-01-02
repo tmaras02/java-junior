bc4fce1a973655b4c7b651c2fda71f733e645ad7
Tomislav Maras <115408239+tmaras02@users.noreply.github.com>
2026-01-02 18:40:15 +0100

    Delete java.junior/src/main/java/hr/abysalto/hiring/api/junior/repository/OrderRepository.java

--- java.junior/src/main/java/hr/abysalto/hiring/api/junior/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-//spremanje, dohvaæanje, brisanje i pronalazak narudžbi
-
-package hr.abysalto.hiring.api.junior.repository;
-
-import hr.abysalto.hiring.api.junior.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
